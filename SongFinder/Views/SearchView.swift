9cc84dabe6e17ed452a13e1f61f9942d7d67bdf4
Morgan <Mharrisstoertz@gmail.com>
2023-04-19 12:05:58 -0400

    Created a scrollable list to display found songs.

--- SongFinder/Views/SearchView.swift
+++ SongFinder/Views/SearchView.swift
@@ -8,8 +8,21 @@
 import SwiftUI
 
 struct SearchView: View {
+    
+    //MARK: stored properties
+    @State var foundSongs: [Song] = []
+    
+    //MARK: computed properties
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List(foundSongs, id: \.trackId) { currentSong in
+            
+            VStack{
+                Text(currentSong.trackName)
+                    .bold()
+                Text(currentSong.artistName)
+                
+            }
+        }
     }
 }
 
