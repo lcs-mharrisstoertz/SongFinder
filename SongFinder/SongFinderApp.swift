f85a6966cc162af9860bc3ef9f5b3e4d10d1712c
Morgan <Mharrisstoertz@gmail.com>
2023-04-19 11:49:17 -0400

    Initial Commit

---
+++ SongFinder/SongFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SongFinderApp.swift
+//  SongFinder
+//
+//  Created by Morgan Harris-Stoertz on 2023-04-19.
+//
+
+import SwiftUI
+
+@main
+struct SongFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
