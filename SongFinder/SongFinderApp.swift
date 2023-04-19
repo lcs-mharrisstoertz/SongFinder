4841414692f8a9fa612347414fd5778474ccd242
Morgan <Mharrisstoertz@gmail.com>
2023-04-19 11:51:54 -0400

    Created project and completed initial organization of views.

--- SongFinder/SongFinderApp.swift
+++ SongFinder/SongFinderApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SongFinderApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            SongDetailView()
         }
     }
 }
---
+++ SongFinder/Views/SongDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  SongDetailView.swift
+//  SongFinder
+//
+//  Created by Morgan Harris-Stoertz on 2023-04-19.
+//
+
+import SwiftUI
+
+struct SongDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SongDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        SongDetailView()
+    }
+}
